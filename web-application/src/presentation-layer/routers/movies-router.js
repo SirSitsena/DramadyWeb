b8d4bc7c803d02bbf2017c909c7d6bb30652672b
SirsSitsena <anestakos322anestis@gmail.com>
2022-02-27 10:32:24 +0100

    Changed the design. Created the html for the SPA and small fixes.

--- web-application/src/presentation-layer/routers/movies-router.js
+++ web-application/src/presentation-layer/routers/movies-router.js
@@ -3,11 +3,13 @@ const movieManager = require('../../business-logic-layer/movie-manager')
 const imdbMovieManager = require('../../business-logic-layer/imdbApi-manager')
 const router = express.Router()
 
-router.get('/', function(request, response) {
-    response.render('home.hbs')
-})
+// router.get('/', function(request, response) {
+// 	console.log("It is Working Main")
+//     response.render('home.hbs')
+// })
 
 router.get("/top250", function(request, response){
+	console.log("It is Working Main")
 	movieManager.getAllMovies(function(errors, top250Movies){
 		const model = {
 			errors: errors,
--- web-application/src/presentation-layer/routers/various-router.js
+++ web-application/src/presentation-layer/routers/various-router.js
@@ -4,10 +4,8 @@ const imdbMovieManager = require('../../business-logic-layer/imdbApi-manager')
 const router = express.Router()
 
 router.get("/", function(request, response){
-
 	response.render("home.hbs")
 })
-
 
 router.get("/about", function(request, response){
 	response.render("about.hbs")
@@ -16,9 +14,11 @@ router.get("/about", function(request, response){
 router.get("/contact", function(request, response){
 	response.render("contact.hbs")
 })
+
 router.get("/extraFeatures", function(request, response){
 	response.render("deadline.hbs")
 })
+
 
 
 
