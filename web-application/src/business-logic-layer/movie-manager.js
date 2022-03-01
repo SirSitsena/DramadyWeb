fc17e904405877af8604109a1497a709d38dd896
Rasmus Kolmodin <rasmus.kolmodin@gmail.com>
2022-03-01 12:07:52 +0100

    Converted projected with awilix for dependency injection.

--- web-application/src/business-logic-layer/movie-manager.js
+++ web-application/src/business-logic-layer/movie-manager.js
@@ -1,16 +1,54 @@
-const movieRepository = require('../data-access-layer/movie-repository')
+//const movieRepository = require('../data-access-layer/movie-repository')
 // const movieValidator = require('./movie-validator')
 
-exports.getAllMovies = function(callback){
-	movieRepository.getAllMovies(callback)
-}
-
-exports.createFavourite = function(movieId, callback){
-	// get current date
-	const date = new Date().toJSON().slice(0, 10)
-	/*
-		TODO: GET CURRENT USER ID, CHECK IF LOGGED IN.
-	*/
-
-	movieRepository.createUserFavourites(userId, movieId, date, callback)
+module.exports = function({movieRepository}){
+	return {
+		getAllMovies: function(callback){
+			movieRepository.getAllMovies(callback)
+		},
+		
+		/* 						FAVOURITES FUNCTIONALITY 					*/
+		
+		viewFavourites: function(accountId, callback){
+			/*
+				Viewing an accounts favourites list.
+			*/
+		},
+		
+		createFavourite: function(movieId, callback){
+			// get current date
+			const date = new Date().toJSON().slice(0, 10)
+			/*
+				TODO: GET CURRENT USER ID, CHECK IF LOGGED IN.
+			*/
+		
+			movieRepository.createUserFavourites(userId, movieId, date, callback)
+		},
+		
+		deleteFavourite: function(movieId, callback){
+			/*
+				Delete from favourites list : TODO
+			*/
+		},
+		
+		/*						 WATCHLIST FUNCTIONALITY						*/
+		
+		viewWatchlist: function(accountId, callback){
+			/*
+				Viewing an accounts watchlist.
+			*/
+		},
+		
+		createWatchlistItem: function(movieId, callback){
+			/*
+				Add to watchlist : TODO
+			*/
+		},
+		
+		deleteWatchlistItem: function(movieId, callback){
+			/*
+				Remove from watchlist : TODO
+			*/
+		}
+	}
 }
